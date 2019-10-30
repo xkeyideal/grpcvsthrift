757f103d4cba925bf10273f0eda5d159ce5f876c
xkey <xk33430@ly.com>
2019-10-30 19:00:48 +0800

    fix

--- rocksdbstore/store.go
+++ rocksdbstore/store.go
@@ -44,11 +44,11 @@ func NewRocksdbStore(dbdir string) (*RocksdbStore, error) {
 	opts.SetLevel0FileNumCompactionTrigger(4)   // Level-0层 当有4个未进行Compact的文件时，达到触发Compact的条件
 	opts.SetLevel0SlowdownWritesTrigger(30)     // Level-0层 当有30个未进行Compact的文件时，触发RocksDB，减慢写入速度
 	opts.SetLevel0StopWritesTrigger(60)         // Level-0层 当有60个未进行Compact的文件时，触发RocksDB停止写入文件，此时会尽快的Compact Level-0层文件
-	opts.SetTargetFileSizeBase(2 * 1024 * 1024) // 2MB
-	opts.SetTargetFileSizeMultiplier(1)
+	opts.SetTargetFileSizeBase(5 * 1024 * 1024) // 5MB Level-0层的数据量压缩的阈值,超过这个值满足触发Compact条件
+	opts.SetTargetFileSizeMultiplier(10)        // Level-N层的数据量压缩的阈值的乘法因子
 
 	// Level-1 层 文件总大小由 max_bytes_for_level_base 参数控制，而 Level-2 层的大小通过： Level_max_bytes[N] = Level_max_bytes[N-1] * max_bytes_for_level_multiplier^(N-1)*max_bytes_for_level_multiplier_additional[N-1]
-	opts.SetMaxBytesForLevelBase(20 * 1024 * 1024) // 20MB 用于指定Level-1层总大小，超过这个值满足触发Compact条件
+	opts.SetMaxBytesForLevelBase(20 * 1024 * 1024) // 20MB 用于指定Level-1层的数据量上限
 	opts.SetMaxBytesForLevelMultiplier(10)         // 每一层最大Bytes 乘法因子
 
 	wo := gorocksdb.NewDefaultWriteOptions()
